b4739f4882ba44e72b2552dc1ad638b309c65b70
TOMAS LANGEBAEK CARRIZOSA <t.langebaek@sis.virtual.uniandes.edu.co>
2019-05-15 12:37:26 -0500

    1.1.1.1.1.1

--- src/model/violations/VOMovingViolation.java
+++ src/model/violations/VOMovingViolation.java
@@ -1,204 +1,159 @@
 package model.violations;
 
-import java.util.Comparator;
-
-public class VOMovingViolation implements Comparable<VOMovingViolation>{
+/**
+ * Representation of a Trip object
+ */
+public class VOMovingViolation {
 	
 
-	private String objectID;
-	private String location;
-	private String ticketIssueDate;
-	private String totalPaid;
-	private String accidentIndicator;
-	private String violationDescription;
-	private String streetSegId;
-	private String addressID;
-	private String xCoord;
-	private String yCoord;
-	private String violationCode;
-	private String latitude;
-	private String longitud;
+	// ---------------------------------------------------------
+	// Atributos
+	// ---------------------------------------------------------
+
+	private String OBJECTID;
+
+	private String LOCATION;
 	
-
-	public VOMovingViolation(String pObjectID, String pLocation, String pTicketIssueDate, String pTotalPaid,
-			String pAccidentIndicator, String pViolatinDescription, String pStreetSegId, String pAddressID
-			, String pXCoord, String pYCoord, String pViolationCode, String pLatitude, String pLongitude) {
+	private String TICKETISSUEDATE;
 	
-		objectID = pObjectID;
-		location = pLocation;
-		ticketIssueDate = pTicketIssueDate;
-		totalPaid = pTotalPaid;
-		accidentIndicator = pAccidentIndicator;
-		violationDescription = pViolatinDescription;
-		streetSegId = pStreetSegId;
-		addressID = pAddressID;
-		xCoord = pXCoord;
-		yCoord = pYCoord;
-		violationCode = pViolationCode;
-		latitude = pLatitude;
-		longitud = pLongitude;
-		
+	private String TOTALPAID;
+	
+	private String ACCIDENTINDICATOR;
+	
+	private String VIOLATIONCODE;
+
+	private String VIOLATIONDESC;
+
+	private String STREETSEGID;
+
+	private String ADDRESS_ID;
+	
+	private String XCOORD;
+
+	private String YCOORD;
+	
+	private String FINEAMT;
+	
+	private String PENALTY1;
+	
+	private String PENALTY2;
+
+	// -------------------------------------------------------
+	// Constructor
+	// -------------------------------------------------------
+	
+	public VOMovingViolation(String pId, String pLocation, String pDate, String pTotalPaid, String indicador,
+			String descripcion, String violationCode, String fineamt, String penalty1, String penalty2, 
+			String addressId, String streetSegId)
+	{
+		this.OBJECTID = pId;
+		this.LOCATION = pLocation;
+		this.TICKETISSUEDATE= pDate;
+		this.TOTALPAID = pTotalPaid;
+		this.ACCIDENTINDICATOR = indicador;
+		this.VIOLATIONDESC = descripcion;
+		this.VIOLATIONCODE = violationCode;
+		this.FINEAMT = fineamt;
+		this.PENALTY1 = penalty1; 
+		this.PENALTY2 = penalty2;
+		this.STREETSEGID = streetSegId;
+		this.ADDRESS_ID = addressId;
 	}
 
-
-	public String getObjectID() {
-		return objectID;
-	}
-
-
-
-	public void setObjectID(String objectID) {
-		this.objectID = objectID;
-	}
-
-
-
-	public String getLocation() {
-		return location;
-	}
-
-
-
-	public void setLocation(String location) {
-		this.location = location;
-	}
-
-
-
-	public String getTicketIssueDate() {
-		return ticketIssueDate;
-	}
-
-
-
-	public void setTicketIssueDate(String ticketIssueDate) {
-		this.ticketIssueDate = ticketIssueDate;
-	}
-
-
-
-	public String getTotalPaid() {
-		return totalPaid;
-	}
-
-
-
-	public void setTotalPaid(String totalPaid) {
-		this.totalPaid = totalPaid;
-	}
-
-
-
-	public String getAccidentIndicator() {
-		return accidentIndicator;
-	}
-
-
-
-	public void setAccidentIndicator(String accidentIndicator) {
-		this.accidentIndicator = accidentIndicator;
-	}
-
-
-
-	public String getViolationDescription() {
-		return violationDescription;
-	}
-
-
-
-	public void setViolationDescription(String violationDescription) {
-		this.violationDescription = violationDescription;
-	}
-
-
-
-	public String getStreetSegId() {
-		return streetSegId;
-	}
-
-
-
-	public void setStreetSegId(String streetSegId) {
-		this.streetSegId = streetSegId;
-	}
-
-
-
-	public String getAddressID() {
-		return addressID;
-	}
-
-
-
-	public void setAddressID(String addressID) {
-		this.addressID = addressID;
-	}
-
-
-
-	public String getxCoord() {
-		return xCoord;
-	}
-
-
-
-	public void setxCoord(String xCoord) {
-		this.xCoord = xCoord;
-	}
-
-
-
-	public String getyCoord() {
-		return yCoord;
-	}
-
-
-
-	public void setyCoord(String yCoord) {
-		this.yCoord = yCoord;
-	}
-
-
-
-	public String getViolationCode() {
-		return violationCode;
-	}
-
-
-
-	public void setViolationCode(String violationCode) {
-		this.violationCode = violationCode;
-	}
-
-
-
-	public String getLatitude() {
-		return latitude;
-	}
-
-
-
-	public void setLatitude(String latitude) {
-		this.latitude = latitude;
-	}
-
-
-
-	public String getLongitud() {
-		return longitud;
-	}
-
-
-
-	public void setLongitud(String longitud) {
-		this.longitud = longitud;
-	}
-
-
-
+	// -------------------------------------------------------
+	// Metodos
+	// -------------------------------------------------------
+
+	
 	@Override
-	public int compareTo(VOMovingViolation arg0) {
-		return this.getViolationCode().compareToIgnoreCase(arg0.getViolationCode());
+	public String toString() {
+		return "VOMovingViolations [objectId()=" + objectId() + ",\n getLocation()=" + getLocation()
+				+ ",\n getTicketIssueDate()=" + getTicketIssueDate() + ",\n getTotalPaid()=" + getTotalPaid()
+				+ ",\n getAccidentIndicator()=" + getAccidentIndicator() + ",\n getViolationDescription()="
+				+ getViolationDescription() + ",\n getStreetSegId()=" + getStreetSegId() + ",\n getAddressId()="
+				+ getAddressId() + "]\n\n";
 	}
+
+
+	/**
+	 * @return id - Identificador único de la infracción
+	 */
+	public String objectId() {
+		return this.OBJECTID;
+	}	
+	
+	public String getViolationCode(){
+		return this.VIOLATIONCODE;
+	}
+	
+	/**
+	 * @return location - Dirección en formato de texto.
+	 */
+	public String getLocation() {
+		return this.LOCATION;
+	}
+
+	/**
+	 * @return date - Fecha cuando se puso la infracción .
+	 */
+	public String getTicketIssueDate() {
+		return this.TICKETISSUEDATE;
+	}
+	
+	/**
+	 * @return totalPaid - Cuanto dinero efectivamente pagó el que recibió la infracción en USD.
+	 */
+	public String getTotalPaid() {
+		return this.TOTALPAID;
+	}
+	
+	/**
+	 * @return accidentIndicator - Si hubo un accidente o no.
+	 */
+	public String  getAccidentIndicator() {
+		return this.ACCIDENTINDICATOR;
+	}
+		
+	/**
+	 * @return description - Descripción textual de la infracción.
+	 */
+	public String  getViolationDescription() {
+		return this.VIOLATIONDESC;
+	}
+	
+	public String getStreetSegId() {
+		return this.STREETSEGID;
+	}
+	
+	public String getAddressId() {
+		return this.ADDRESS_ID;
+	}
+	
+	public String getXCoord() {
+		return this.XCOORD;
+	}
+	
+	public String getYCoord() {
+		return this.YCOORD;
+	}
+	
+	public String getFineAMT() {
+		return this.FINEAMT;
+	}
+	
+	public String getPenalty1() {
+		return this.PENALTY1;
+	}
+	
+	public String getPenalty2() {
+		return this.PENALTY2;
+	}
+	
+	public String TOSTRING(){
+		System.out.println(VIOLATIONCODE);
+		System.out.println(PENALTY1);
+		System.out.println(STREETSEGID);
+		return "";
+	}
+
 }
